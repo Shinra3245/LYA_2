e3121816fa2044b3e6eb5f16d5c4332aa878ddb5
Shinra3245 <weplay3245@gmail.com>
2026-07-01 13:37:12 -0600

    feat:nuevos animes en la lista

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-$data = ["Demon slayer", "Attack on Titan", "Jujutsu Kaisen", "One Piece", "My Hero Academia", "Tokyo Revengers", "Chainsaw Man", "Black Clover];
+$data = ["Demon slayer", "Attack on Titan", "Jujutsu Kaisen", "One Piece", "My Hero Academia", "Tokyo Revengers", "Chainsaw Man", "Black Clover"];
 ?>
